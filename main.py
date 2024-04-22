ecf9d4ccd63fc85c2fe7e4b4bc6e668242b11f65
Joshua Scarsbrook <jscarsbrook@gmail.com>
2024-04-22 13:30:17 +1000

    added new version using python-cvmfsutils

--- main.py
+++ main.py
@@ -4,176 +4,95 @@ Joshua D. Scarsbrook - The University of Queensland
 License: Apache-2.0
 """
 
-import urllib.parse
-import urllib
-import requests
-import os.path
-import zlib
-import sqlite3
-from struct import pack
-import csv
-import hashlib
-from subprocess import check_output
+import cvmfs
 import sys
+import os
+import os.path
+import sqlite3
+import time
 
 
-def fetch_http_file(url):
-    u = urllib.parse.urlparse(url)
+def index_repo(rev, output):
+    db = sqlite3.connect(output + ".tmp")
 
-    # Create a local cache path.
-    path = os.path.join("cache", u.netloc, u.path[1:] + ".cache")
+    # Create the catalog table.
+    db.execute(
+        """CREATE TABLE catalog (
+            md5path_1 INTEGER,
+            md5path_2 INTEGER,
+            parent_1 INTEGER,
+            parent_2 INTEGER,
+            hash BLOB,
+            name TEXT
+        )"""
+    ).close()
 
-    # If the path already exists then return the existing file.
-    if os.path.exists(path):
-        return path
+    for clg in rev.catalogs():
+        print(clg)
 
-    # Create all directories in the tree.
-    dirname = os.path.dirname(path)
-    os.makedirs(dirname, exist_ok=True)
+        res = clg.run_sql(
+            "SELECT md5path_1, md5path_2, parent_1, parent_2, hash, name FROM catalog"
+        )
 
-    print("downloading: %s", url)
+        for md5path_1, md5path_2, parent_1, parent_2, content_hash, name in res:
+            db.execute(
+                "INSERT INTO catalog(md5path_1, md5path_2, parent_1, parent_2, hash, name) VALUES (?, ?, ?, ?, ?, ?)",
+                (md5path_1, md5path_2, parent_1, parent_2, content_hash, name),
+            ).close()
 
-    # Download the file.
-    resp = requests.get(url)
-    resp.raise_for_status()
+    db.commit()
 
-    with open(path, "wb") as f:
-        f.write(resp.content)
+    db.close()
 
-    return path
+    print("finished indexing")
 
+    time.sleep(5)
 
-def fetch_http_bytes(url):
-    return open(fetch_http_file(url), "rb").read()
-
-
-def data_url_for(base_url, hash):
-    # Given a hash get the URL for the file contents.
-    return "{}data/{}/{}C".format(base_url, hash[:2], hash[2:])
-
-
-def sha1(content):
-    return hashlib.sha1(content).hexdigest()
-
-
-def get_all_files_in_catalog(base_url, hash, ret):
-    catalog_url = data_url_for(base_url, hash)
-
-    catalog_file = fetch_http_file(catalog_url)
-
-    # Decompress the catalog data.
-    contents = zlib.decompress(open(catalog_file, "rb").read())
-
-    with open(catalog_file + ".sqlite", "wb") as f:
-        f.write(contents)
-
-    # Open the sqlite database.
-    catalog = sqlite3.connect(catalog_file + ".sqlite")
-
-    # For every file in the catalog add it to the index.
-    for p1, p2, parent1, parent2, name, content_hash in catalog.execute(
-        "SELECT md5path_1, md5path_2, parent_1, parent_2, name, hash FROM catalog"
-    ):
-        path = pack(">qq", p1, p2).hex()
-        parent = pack(">qq", parent1, parent2).hex()
-
-        if content_hash != None:
-            content_hash = content_hash.hex()
-        else:
-            content_hash = "0000000000000000000000000000000000000000"
-
-        ret[path] = (parent, content_hash, name)
-
-    print("loaded ", len(ret), " records")
-
-    # Recurse into any nested catalogs.
-    for path, sha1 in catalog.execute("SELECT path, sha1 FROM nested_catalogs"):
-        get_all_files_in_catalog(base_url, sha1, ret)
-
-    return ret
+    os.rename(output + ".tmp", output)
 
 
-results_cache = {}
+def get_path(db: sqlite3.Connection, parent_1, parent_2):
+    if parent_1 == 0 and parent_2 == 0:
+        return ""
 
+    results = db.execute(
+        "SELECT parent_1, parent_2, name FROM catalog WHERE md5path_1 = ? AND md5path_2 = ? LIMIT 1",
+        (parent_1, parent_2),
+    )
 
-def do_search(hash, kind=""):
-    out = []
+    if results.rowcount == 0:
+        raise Exception("not found")
 
-    if hash in results_cache:
-        out = results_cache[hash]
-    else:
-        # Search by calling grep to find the hash.
-        out = check_output(["grep", hash, "db.csv"]).decode("utf8").splitlines()
-        results_cache[hash] = out
-
-    # We assume there are no full hash collisions so recurse on any lines in the output.
-    ret = []
-    for line in out:
-        path_hash, parent_hash, content_hash, name = line.split(",")
-        if kind == "parent" and parent_hash == hash:
-            ret.append((path_hash, parent_hash, content_hash, name))
-        elif kind == "content" and content_hash == hash:
-            ret.append((path_hash, parent_hash, content_hash, name))
-        elif kind == "path" and path_hash == hash:
-            ret.append((path_hash, parent_hash, content_hash, name))
-
-    return ret
-
-
-def get_path(hash, first=True):
-    # Build the final path of the file.
-
-    kind = "content"
-    if not first:
-        kind = "path"
-
-    results = do_search(hash, kind=kind)
-
-    ret = []
-
-    print(results)
-
-    for path_hash, parent_hash, content_hash, name in results:
-        if parent_hash == "00000000000000000000000000000000":
-            ret.append("/")
-            continue
-        child_results = get_path(parent_hash, first=False)
-        for result in child_results:
-            ret.append(os.path.join(result, name))
-
-    return ret
+    for parent_1, parent_2, name in results:
+        parent = get_path(db, parent_1, parent_2)
+        return os.path.join(parent, name)
 
 
 def main(args):
-    if args[1] == "search":
-        # Search for a given content hash.
-        print(get_path(args[2]))
-    elif args[1] == "index":
-        # Index a existing CVMFS repository. Pass a URL to access the repo at.
-        base_url = args[2]
+    repo_url, data_hash = args[0].split("/data/")
+    data_hash = data_hash.replace("/", "")
 
-        # Parse the cvmfspublished file to get the catalog hash.
-        published_file = fetch_http_bytes(base_url + ".cvmfspublished")
-        published = published_file.split(b"\n--\n")[0].decode("utf8").splitlines()
-        published = {k[0]: k[1:] for k in published}
+    print(repo_url, data_hash)
 
-        ret = {}
+    repo = cvmfs.open_repository(repo_url)
 
-        # Get all files in the catalog.
-        files = get_all_files_in_catalog(base_url, published["C"], ret)
+    revision = repo.get_current_revision()
 
-        print("finished loading")
+    database_cache = os.path.join("cache", revision.root_hash + ".db")
+    if not os.path.exists(database_cache):
+        index_repo(revision, database_cache)
 
-        # Write the index to a CSV file for later searching.
-        with open("db.csv", "w") as f:
-            w = csv.writer(f)
-            for k in files:
-                w.writerow([k] + list(files[k]))
+    content_hash = bytes.fromhex(data_hash)
 
-        print("finished")
-    else:
-        print("usage: search_by_hash.py [search <hash>|index <repo>]")
+    db = sqlite3.connect(database_cache)
+
+    for parent_1, parent_2, name in db.execute(
+        "SELECT parent_1, parent_2, name FROM catalog WHERE hash = ?",
+        (content_hash,),
+    ):
+        path = get_path(db, parent_1, parent_2)
+        print(os.path.join(path, name))
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main(sys.argv[1:])
